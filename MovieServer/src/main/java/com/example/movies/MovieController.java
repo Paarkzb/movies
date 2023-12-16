46566248c7f94ad667ee97f64ebb51bc0cc18012
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2023-12-16 17:16:12 +0800

    init

---
+++ MovieServer/src/main/java/com/example/movies/MovieController.java
@@ -0,0 +1,25 @@
+package com.example.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.findMovieByImdbID(imdbId), HttpStatus.OK);
+    }
+}
